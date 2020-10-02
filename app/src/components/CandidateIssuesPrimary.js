4218b663afb161e67d065835b163cf117c6ece7f
eidietrich <eric.dietrich@gmail.com>
2020-10-02 15:15:42 -0600

    Cleanup

--- app/src/components/CandidateIssuesPrimary.js
+++ app/src/components/CandidateIssuesPrimary.js
@@ -5,7 +5,11 @@ import TextBlock from '../library/TextBlock.js'
 
 import styles from './CandidateIssues.module.css'
 
-const ledeIn = `Issue statements were solicited from active candidates via a written questionnaire before the June primary election. Answers have been lightly edited for punctuation and spelling.`
+const hed = 'On the issues'
+const ledeIn = `Issue statements were solicited from active candidates via a written questionnaire before the June primary election. Answers were lightly edited for punctuation and spelling.`
+
+const excludedRaceNote = `Due to time constraints, MTFP didn't conduct a candidate questionnaire for this race before the June primary.`
+const noAnswerNote = `No responses on file to questionnaire conducted before June primary. {ledeIn} All active candidates in the race were contacted repeatedly via phone numbers and emails included in election paperwork. Some didn't submit complete responses by deadline.`
 
 const CandidateIssues = (props) => {
     const { color, race } = props
@@ -13,15 +17,15 @@ const CandidateIssues = (props) => {
 
     if ((race.hasIssueStatements === 'no')) {
         return <div className={styles.container}>
-            <h2>Primary questionnaire responses</h2>
-            <div className={'note'}>Due to time constraints, MTFP didn't conduct a candidate questionnaire for this race before the June primary.</div>
+            <h2>{hed}</h2>
+            <div className={'note'}>{excludedRaceNote}</div>
         </div>
     }
 
     if (!responses) {
         return <div className={styles.container}>
-            <h2>Primary questionnaire responses</h2>
-            <div className={'note'}>No responses on file to questionnaire conducted before June primary. {ledeIn} All active candidates in the race were contacted repeatedly via phone numbers and emails included in election paperwork. Some didn't submit complete responses by deadline.</div>
+            <h2>{hed}</h2>
+            <div className={'note'}>{noAnswerNote}</div>
         </div>
     }
 
@@ -33,7 +37,7 @@ const CandidateIssues = (props) => {
     })
     return (
         <div className={styles.container}>
-            <h2>Primary questionnaire responses</h2>
+            <h2>{hed}</h2>
             <div className={'note'}>{ledeIn}</div>
             <div className={styles.responses}>{responsesRendered}</div>
         </div>
